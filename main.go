396f48786ff393e6b1e1a7f963c277a307bc7485
pancho horrillo <pancho.horrillo@bbva.com>
2020-06-30 15:44:03 +0200

    chore: testing hjr265/ptrace.go
    See:
    https://github.com/hjr265/ptrace.go/blob/master/ptrace/ptrace_test.go
    https://pkg.go.dev/syscall?tab=doc#PtraceAttach
    
    Co-authored-by: Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>

--- main.go
+++ main.go
@@ -3,19 +3,43 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"syscall"
+	"runtime"
 
-	"github.com/tfogal/ptrace"
+	"github.com/hjr265/ptrace.go/ptrace"
 )
 
 func main() {
 	argv := []string{"./test-minimal"}
-	inferior, err := ptrace.Exec(argv[0], argv)
 
+	runtime.LockOSThread()
+	proc, err := os.StartProcess(argv[0], argv, &os.ProcAttr{
+		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
+		Sys: &syscall.SysProcAttr{
+			Ptrace:    true,
+			Pdeathsig: syscall.SIGCHLD,
+		},
+	})
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	defer inferior.Close()
+	state, err := proc.Wait()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	fmt.Println(inferior)
+	tracer, err := ptrace.Attach(proc)
+
+	fmt.Printf("%+v\n%+v\n%+v\n%+v\n", proc, state, tracer, err)
+
+
+	for {
+		syscall_number, err := tracer.Syscall(0)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Printf("syscall_number: %+v\n", syscall_number)
+	}
 }
