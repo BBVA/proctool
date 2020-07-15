84981955d6cf36efd40f579a57e7439a23422371
pancho horrillo <pancho.horrillo@bbva.com>
2020-07-15 17:59:35 +0200

    Misc fixes and clarifications
    Co-authored-by: Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>

--- hijo-correveidile/main.go
+++ hijo-correveidile/main.go
@@ -1,3 +1,9 @@
+// TODO: Honour an env var DEBUG that triggers that all events are dumped to a log/logs
+// events will be identified across different logs via an autoincremented index, which
+// should be expressed as a fixed width hex number
+// There can be a log that maps event numbers with timestamps, to enable human understanding of the fenomenon
+
+
 package main
 
 import (
@@ -8,6 +14,7 @@ import (
     "crypto/md5"
     "io"
     "runtime"
+    "strings"
     "syscall"
 
     sec "github.com/seccomp/libseccomp-golang"
@@ -39,6 +46,13 @@ func main() {
         if err != nil {
             log.Fatalln(err)
         }
+        // TODO: this process should return the exit code of the spawned command
+        // and the other branch should capture it and return it to the caller
+        // so that the CI gets the same information it would from the original spawned command
+
+        // TODO: don't use stdin/stdout, but write to log files, so that we keep the expected output unaltered
+
+        // TODO: 
     } else {
         runtime.LockOSThread()
         log.Printf("%+v\n", os.Args)
@@ -50,7 +64,7 @@ func main() {
                 Sys: &syscall.SysProcAttr{
                     Ptrace: true,
                 },
-                Env: []string{"GOGC=off"},
+                // Env: []string{"GOGC=off"},
             },
         )
         if err != nil {
@@ -88,22 +102,25 @@ func main() {
 
         log.Println("Corre Sara Connor")
 
+        // TODO: review what is the absolute minimum we need to remember
+        // Maybe we can try to implement this without a map
         regs_of := make(map[int]*syscall.PtraceRegs)
 
         c := make(chan int)
 
         hashFileAndContinue := func(path string, pid int) {
-            hash, err := hashFile(path)
-            if err != nil {
-                log.Fatalln(err)
+            if !strings.HasPrefix(path, "/dev") && path != "" {
+                hash, err := hashFile(path)
+                if err != nil {
+                    log.Println(err)
+                }
+                log.Printf("pid: %d, path: %q, hash: %q\n", pid, path, hash)
             }
-            log.Printf("pid: %d, path: %q, hash: %q\n", pid, path, hash)
-            // TODO: not getting through
             err = syscall.Kill(proctooled.Pid, syscall.SIGUSR1)
             if err != nil {
                 log.Fatalln(err)
             }
-            // proctooled.Signal(syscall.SIGUSR10)
+            // NOTES: does not work! proctooled.Signal(syscall.SIGUSR1)
             c<-pid
             log.Println("Reported to channel")
         }
@@ -132,6 +149,8 @@ func main() {
                 // TODO: ensure that the sender of the signal is one of our goroutines (sender shares TGL with us)
                 if (wstatus.StopSignal() == syscall.SIGUSR1) {
                     log.Println("Trying to read from channel n.5")
+                    // TODO: this loop was made out of fear of losing repeated signals that don't pile up
+                    // Maybe we can get rid of it; we must test if signals are lost or not
                     FOR:
                     for {
                         select {
@@ -155,7 +174,7 @@ func main() {
                 continue
             }
 
-            log.Println("Nietecito!")
+            log.Println("Ay mi nietecito!")
 
             if wstatus.StopSignal() == syscall.SIGTRAP && wstatus.TrapCause() == 0 {
                 regs := &syscall.PtraceRegs{}
@@ -169,6 +188,8 @@ func main() {
                     }
                     regs_of[pid] = regs
                     switch syscall_number := regs.Orig_rax; syscall_number {
+                    case syscall.SYS_OPENAT:
+                        log.Printf("TUUUUUUUU!\n   regs:%+v\n", regs)
                     case syscall.SYS_EXECVE:
                         path, err := readString(pid, uintptr(regs.Rdi))
                         if err != nil {
@@ -190,7 +211,13 @@ func main() {
                         } else if regs.R10&syscall.O_WRONLY != 0 {
 
                         } else { // O_RDONLY
-                            if int64(regs.Rax) != -1 {
+                            regs2 := &syscall.PtraceRegs{}
+                            err = syscall.PtraceGetRegs(pid, regs2)
+                            if err != nil {
+                                log.Fatalln(err)
+                            }
+                            if int64(regs2.Rax) >= 0 {
+                                log.Printf("YOOOOOOOO!\n   regs:%+v\n  regs2:%+v\n", regs, regs2)
                                 // TODO: what happens if child or grandchild is running in a chroot? or docker?
                                 // the recovered path might not make sense to the tracer
                                 // idea: /proc/pid/fd points to the actual file, no matter what
@@ -228,7 +255,6 @@ func main() {
                 }
             }
         }
-
     }
 }
 
